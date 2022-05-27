b66b9ccf18a4fab896519fe15a7909ff35dfff4f
Brassy <dsgames737@gmail.com>
2022-05-27 17:56:05 +0300

    1.0.4

---
+++ js/dscript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+   $('.homepageslider').slick({
+      infinite: true,
+      arrows:false,
+      slidesToShow: 4,
+      responsive: [{
+         
+      }]
+   });
+});
+
+$(document).ready(function () {
+   $('.slider-news').slick({
+      infinite: true,
+      fade:true,
+      dots:true,
+      arrows: false,
+      slidesToShow: 1,
+
+      responsive: [{
+
+      }]
+   });
+});
