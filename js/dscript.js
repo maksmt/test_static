53d5402d64efdc03b65c125aadacf5082e23eb44
Brassy <dsgames737@gmail.com>
2022-06-01 15:23:50 +0300

    1.4

--- js/dscript.js
+++ js/dscript.js
@@ -4,8 +4,29 @@ $(document).ready(function() {
       arrows:false,
       slidesToShow: 4,
       responsive: [{
+         breakpoint: 1191,
+         settings:{
+            slidesToShow:3
+         }
+         },
+         {
+            breakpoint: 992,
+            settings: {
+               slidesToShow:2,
+            }
+         },
+         {
+            breakpoint: 600,
+            settings:{
+               slidesToShow:1,
+               variableWidth: true,
+               centerMode: true
+            }
+         },
+      
          
-      }]
+         
+   ]
    });
 });
 
@@ -18,7 +39,65 @@ $(document).ready(function () {
       slidesToShow: 1,
 
       responsive: [{
-
+         breakpoint: 550,
+         settings:{
+            dots:false
+         }
       }]
    });
+});
+$(document).ready(function () {
+   $('.rayon-slider').slick({
+      slidesToShow: 4,
+      arrows:false,
+      responsive: [{
+         breakpoint: 1191,
+         settings:{
+            slidesToShow:3
+         }
+      },
+      {
+         breakpoint: 992,
+         settings:{
+            slidesToShow:2
+         }
+      }, 
+      {
+         breakpoint: 600,
+         settings:{
+            slidesToShow:1
+         }
+      }
+   ]
+   });
+});
+
+$(document).ready(function (){
+   $('.slider-baryga-photo').slick({
+      slidesToShow:2,
+      arrows: false,
+      dots:false,
+      responsive:[
+         {
+            breakpoint: 992, 
+            settings: {
+               slidesToShow: 1
+            }
+         },
+      ]
+   });
+});
+
+$(".contact-pop-it").on("click", function(){
+   $(".pop-up").attr('style', 'display:block;');
+});
+$(".pop-up").on("click", function(){
+   $(".pop-up").attr('style', 'display:none;');
+});
+
+$(".gallery").on("click", function(){
+   $(".gallerycon").attr('style', 'display:flex;');
+});
+$(".closebutton").on("click", function(){
+   $(".gallerycon").attr('style', 'display:none;');
 });
